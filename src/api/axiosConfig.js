2c2c4a5323a3655d083a05ea2062e9eac321a191
Manpreet4491 <129290161+Manpreet4491@users.noreply.github.com>
2024-06-09 15:46:29 +0530

    final

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'movies-production-ab74.up.railway.app',
+    baseURL:'https://movies-production-ab74.up.railway.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
